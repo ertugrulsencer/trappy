ed3e8d3fc9523a29b8b07bdfbd8e351b9b9a2495
Ertugrul <teknoertugrul@gmail.com>
2021-05-13 22:45:51 +0300

    User controller added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,26 @@
-const getUsers = (limit = false) => {
-  return [];
+const User = require("../models/user");
+const getUsers = (req, res) => {
+  res.send("test");
+};
+const addUser = (req, res) => {
+  const insertUser = new User({
+    user_name: "ertugrulsencer",
+    user_full_name: "Ertuğrul Sencer",
+    user_email: "teknoertg@gmail.com",
+    user_password: "hdkfkfkfkfkfkfuryskcmdhstwoaspcm",
+  });
+  insertUser
+    .save()
+    .then((res) => {
+      console.log(res);
+      res.send(res);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send(err);
+    });
 };
 module.exports = {
   getUsers,
+  addUser,
 };
