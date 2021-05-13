a274baeccf942d41f648d9b8c9a52cf13e925436
Ertugrul <teknoertugrul@gmail.com>
2021-05-13 22:46:36 +0300

    User routes controller added

--- server/router/api.js
+++ server/router/api.js
@@ -5,12 +5,17 @@ const {
   updateSeo,
   deleteSeo,
 } = require("../controllers/seoController");
+const { getUsers, addUser } = require("../controllers/userController");
 
+/* Seo */
 router.get("/seo/:page", getSeo);
 router.get("/seo", getSeo);
 router.post("/seo", addSeo);
 router.put("/seo/:id", updateSeo);
 router.delete("/seo/:id", deleteSeo);
+/* User */
+router.get("/users", getUsers);
+router.post("/new-user", addUser);
 
 // 404
 router.get("*", (req, res) => {
